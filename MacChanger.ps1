9e93c495ef159259ba4049dcc296f569adfcfe29
t3hbb <dispacct@hotmail.com>
2020-02-22 15:47:11 +0000

    Initial Commit

---
+++ MacChanger.ps1
@@ -0,0 +1,12 @@
+﻿foreach($line in [System.IO.File]::ReadLines("***filename containing MAC addressess***"))
+{
+    #Echo "Setting Adapter to MAC address $line"
+    $ethname = "Ethernet 3" //change to the correct addapter
+    Set-NetAdapter -Name "Ethernet 3" -MacAddress $line -Confirm:$false
+    Restart-NetAdapter -Name $ethname
+    Start-Sleep -Seconds 50
+    $ip = (Get-NetIPAddress -InterfaceAlias $ethname -AddressFamily IPv4).IPv4Address
+    if($ip.Split(".")[0] -ne "169"){
+        Write-Host "Issued IP Address $ip for MAC address : $line" -ForegroundColor RED
+        }
+ }
